d42376926e24780e7f14fca030f11b056f7dc0b5
Jingyu Zou <jzou@netflix.com>
2025-08-17 14:42:38 -0400

    cosmetic

--- cs336_basics/linear.py
+++ cs336_basics/linear.py
@@ -9,16 +9,16 @@ class Linear(torch.nn.Module):
 
     def __init__(self, d_in: int, d_out: int, device=None, dtype=None):
         super().__init__()
-        w: Float[Tensor, "d_out d_in"] = torch.empty((d_out, d_in), device=device, dtype=dtype)
+        self.weight: Float[Tensor, "d_out d_in"] = torch.nn.Parameter(
+            torch.empty((d_out, d_in), device=device, dtype=dtype))
         sigma = math.sqrt(2 / (d_in + d_out))
         torch.nn.init.trunc_normal_(
-            w,
+            self.weight,
             mean=0.0,
             std=sigma,
             a=-3.0 * sigma,
             b=3.0 * sigma,
         )
-        self.weight = torch.nn.Parameter(w, requires_grad=True)
 
     def forward(self, in_features: Float[Tensor, " ... d_in"]) -> Float[Tensor, " ... d_out"]:
         return einsum(self.weight, in_features, "d_out d_in, ... d_in -> ... d_out")
