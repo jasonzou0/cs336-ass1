08b4a31ae7860ddd826ca91cf472c4a6e77e7740
Jingyu Zou <jzou@netflix.com>
2025-08-29 23:16:58 -0500

    softmax

--- cs336_basics/module/__init__.py
+++ cs336_basics/module/__init__.py
@@ -4,3 +4,4 @@ from .norm import RmsNorm
 from .swiglu import SwiGLU
 from .silu import silu
 from .rope import Rope
+from .softmax import softmax
--- tests/adapters.py
+++ tests/adapters.py
@@ -13,7 +13,7 @@ from einops import einsum
 # Import from the proper cs336_basics package
 from cs336_basics.train_bpe import train_bpe
 from cs336_basics.tokenizer import Tokenizer
-from cs336_basics.module import Linear, Embedding, RmsNorm, silu, SwiGLU, Rope
+from cs336_basics.module import Linear, Embedding, RmsNorm, silu, SwiGLU, Rope, softmax
 
 
 def run_linear(
@@ -449,7 +449,7 @@ def run_softmax(in_features: Float[Tensor, " ..."], dim: int) -> Float[Tensor, "
         Float[Tensor, "..."]: Tensor of with the same shape as `in_features` with the output of
         softmax normalizing the specified `dim`.
     """
-    raise NotImplementedError
+    return softmax(in_features, dim=dim)
 
 
 def run_cross_entropy(
